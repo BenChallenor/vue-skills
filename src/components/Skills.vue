b9c67238f524b11dbae254a64d2c0e9f04efa18a
Ben <ben.challenor@btopenworld.com>
2018-10-07 17:06:16 +0100

    Add result validator

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -48,8 +48,15 @@ export default {
   },
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill}),
-      this.skill = ''
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill}),
+          this.skill = ''
+        }else {
+          console.log("Not valid");
+        }
+      })
+
     }
   }
 }
@@ -104,5 +111,13 @@ input {
   font-size: 1.3em;
   background-color: #323333;
   color: #687F7F;
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
 }
 </style>
